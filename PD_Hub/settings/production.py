11e55f89efefb70353f7828442a391a9adf94c71
etapau <etapau@gmail.com>
2015-03-26 13:31:01 -0400

    Readme Update

--- PD_Hub/settings/production.py
+++ PD_Hub/settings/production.py
@@ -7,7 +7,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+DATABASES = {'default': dj_database_url.config()}
 
 #Env Variables
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PD_Hub.settings.production")
