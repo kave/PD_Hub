5201d260be0f1355be54327d9b791c61f318aad5
etapau <etapau@gmail.com>
2015-03-26 13:05:29 -0400

    heroku db url

--- PD_Hub/settings/production.py
+++ PD_Hub/settings/production.py
@@ -7,6 +7,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('HEROKU_POSTGRESQL_GRAY_URL'))}
+
 #Env Variables
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PD_Hub.settings.production")
 os.environ.setdefault("NPM_CONFIG_PRODUCTION", "true")
