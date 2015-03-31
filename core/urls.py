1eeca5070fc42025b726105bc69423019fc116a5
etapau <etapau@gmail.com>
2015-03-31 12:51:15 -0400

    PDP detail view with temporary profile form for PDP creation

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from core.views import PDPlanList
+from core.views import *
 
 urlpatterns = patterns('',
 
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^login/$', 'core.views.user_login', name='login'),
     url(r'^register/$', 'core.views.register', name='register'),
     url(r'^profile/$', 'core.views.profile', name='profile'),
-    url(r'^list/$', PDPlanList.as_view()),
+    url(r'^list/$', PDPlanList.as_view(), name='pdp_list'),
+    url(r'^view/(?P<pk>\d+)/$', PDPPlanDetailView.as_view(), name='pdp_view'),
 )
