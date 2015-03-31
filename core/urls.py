c1d3df346a1c749e5597fd5ee2834e1ddeff4a62
Mike Brown <mike.brown@excella.com>
2015-03-31 14:14:05 -0400

    Rename PDPPlan to PDPlan

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     url(r'^profile/$', 'core.views.profile', name='profile'),
     url(r'^create_pdplan/$', PDPlanCreateView.as_view()),
     url(r'^list/$', PDPlanList.as_view(), name='pdp_list'),
-    url(r'^view/(?P<pk>\d+)/$', PDPPlanDetailView.as_view(), name='pdp_view'),
+    url(r'^view/(?P<pk>\d+)/$', PDPlanDetailView.as_view(), name='pdp_view'),
 )
