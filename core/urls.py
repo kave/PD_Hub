60f2fc3fc73e65ab39cdf26e0b72956e016c83db
slippage <dgjohnson1@gmail.com>
2015-03-31 13:52:05 -0400

    urls fixed

--- core/urls.py
+++ core/urls.py
@@ -3,18 +3,7 @@ from core.views import *
 
 urlpatterns = patterns('',
 
-<<<<<<< HEAD
-    
-    url(r'^login/$', 'core.views.user_login', name='login'),
-    url(r'^register/$', 'core.views.register', name='register'),
-    url(r'^profile/$', 'core.views.profile', name='profile'),
 
-    url(r'^$', PDPlanList.as_view()),
-
-    url(r'^list/$', PDPlanList.as_view(), name='pdp_list'),
-    url(r'^view/(?P<pk>\d+)/$', PDPPlanDetailView.as_view(), name='pdp_view'),
-
-=======
     url(r'^login/$', 'core.views.user_login', name='login'),
     url(r'^register/$', 'core.views.register', name='register'),
     url(r'^create_pdplan/$', PDPlanCreateView.as_view()),
@@ -22,5 +11,4 @@ urlpatterns = patterns('',
     url(r'^profile/$', PDPlanList.as_view(), name='profile'),
     url(r'^list/$', PDPlanList.as_view(), name='pdp_list'),
     url(r'^view/(?P<pk>\d+)/$', PDPPlanDetailView.as_view(), name='pdp_view'),
->>>>>>> 82380bbabb3ccd59fc2f0a7d115e25cdc52c3501
 )
